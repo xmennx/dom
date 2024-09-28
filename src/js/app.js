f3876c57f0770ce2921dc5a156e83bdf0240a67e
Гурьянов Вячеслав <pirass1@mail.ru>
2024-09-28 19:47:51 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+// TODO: write code here
+import GameController from './GameController';
+
+const gameController = new GameController(document.querySelector('.field'));
+
+gameController.nextTurn();
